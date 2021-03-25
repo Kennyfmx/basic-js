0ac14240e7936b83d5320c2a78617b1043455e7b
kennyfmx <fmxhighlife@gmail.com>
2021-03-26 00:01:48 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Eroro');
+  }
+  let transformed = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch(arr[i]) {
+      case "--discard-next":
+        i++;
+        continue;
+      case "--discard-prev":
+        if (arr[i - 2] != "--discard-next") {
+        transformed.pop();
+        }
+        continue;
+      case "--double-next":
+        if (arr[i + 1] != undefined) {
+          transformed.push(arr[i + 1]);
+        }
+        continue;
+      case "--double-prev":
+        if (arr[i - 2] != "--discard-next" && arr[i - 1] != undefined) {
+          transformed.push(transformed[transformed.length - 1]);
+        }
+        continue;
+      default: transformed.push(arr[i]);
+    }
+  }
+  return transformed;
+}
