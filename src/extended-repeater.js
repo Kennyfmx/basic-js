3d09875feb04ef94f7f9a33e9652fe8b584f7144
kennyfmx <fmxhighlife@gmail.com>
2021-03-28 21:49:37 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.separator === undefined || options.separator === null) {
+    options.separator = '+';
+  }
+  if (options.additionSeparator === undefined || options.additionSeparator === null) {
+    options.additionSeparator = '|';
+  }
+  if (options.repeatTimes === undefined || options.repeatTimes === null) {
+    options.repeatTimes = 1;
+  }
+  if (options.additionRepeatTimes === undefined || options.additionRepeatTimes=== null) {
+    options.additionRepeatTimes = 1;
+  }
+
+  let repeated = [];
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    repeated.push(String(str));
+    if (options.addition !== undefined && options.additionRepeatTimes > 0) {
+      repeated.push(repeater(
+        options.addition,
+        {
+          separator: options.additionSeparator,
+          repeatTimes: options.additionRepeatTimes,
+        }
+      ))
+    }
+    if (i != options.repeatTimes - 1) {
+      repeated.push(options.separator);
+    }
+  }
+  return repeated.join('');
 };
   
